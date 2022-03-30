6faaef025701c2324892c6339a7bfd72e4cc5131
Stephen Walsh <Stephenfionnwalsh@gmail.com>
2022-03-30 20:47:18 +0200

    start node course

---
+++ 4-natours/starter/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'hello from the server' });
+});
+
+app.post('/', (req, res) => {
+  res.send('you can post to this endpoint');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`listening at port ${port}`);
+});
